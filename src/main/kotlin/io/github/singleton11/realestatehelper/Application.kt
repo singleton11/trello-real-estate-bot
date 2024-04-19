e7284d9d43a55fbeed4a686b3adbb7b95dae22e3
Anton Prokhorov <anton.prokhorov@jetbrains.com>
2024-04-20 00:18:43 +0200

    Add basic route

--- src/main/kotlin/io/github/singleton11/realestatehelper/Application.kt
+++ src/main/kotlin/io/github/singleton11/realestatehelper/Application.kt
@@ -6,5 +6,5 @@ import io.ktor.server.netty.*
 fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
-
+    configureRouting()
 }
---
+++ src/main/kotlin/io/github/singleton11/realestatehelper/Routing.kt
@@ -0,0 +1,13 @@
+package io.github.singleton11.realestatehelper
+
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello world")
+        }
+    }
+}
